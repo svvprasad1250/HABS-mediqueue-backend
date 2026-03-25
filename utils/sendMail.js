bd39c1948b6be525bdecdf807d4db06bf58d9638
svvprasad1250 <venakataprasad5@gmail.com>
2026-03-25 19:37:59 +0530

    nodemailer update ipv4

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -2,7 +2,10 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, htmlContent) => {
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host:"smtp/gmail.com",
+        port:587,
+        secure:false,
+        family:4,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
