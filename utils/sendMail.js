13c612b03d812f7fa79840f9044eb360cfd222c0
svvprasad1250 <venakataprasad5@gmail.com>
2026-03-25 19:53:53 +0530

    force ipv4

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,11 +1,13 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first"); // ✅ FORCE IPv4
+
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, htmlContent) => {
     const transporter = nodemailer.createTransport({
-        host:"smtp.gmail.com",
-        port:587,
-        secure:false,
-        family:4,
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
