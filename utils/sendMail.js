351f9fb5c279900ce6222c44bb6d3082c512021d
svvprasad1250 <venakataprasad5@gmail.com>
2026-03-25 21:01:55 +0530

    brevo mail add

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,16 +1,32 @@
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const sendEmail = async (to, subject, htmlContent) => {
-    const response = await resend.emails.send({
-        from: "onboarding@resend.dev", // ✅ MUST USE THIS
-        to: to,
+    const client = SibApiV3Sdk.ApiClient.instance;
+
+    const apiKey = client.authentications["api-key"];
+    apiKey.apiKey = process.env.BREVO_API_KEY;
+
+    const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    const sender = {
+        email: "venkatprashu008@gmail.com", // 👈 YOU CAN USE YOUR GMAIL HERE
+        name: "MediQueue"
+    };
+
+    const receivers = [
+        {
+            email: to
+        }
+    ];
+
+    await tranEmailApi.sendTransacEmail({
+        sender,
+        to: receivers,
         subject: subject,
-        html: htmlContent
+        htmlContent: htmlContent
     });
 
-    console.log("Email response:", response); // 👈 add this for debug
+    console.log("✅ Email sent successfully");
 };
 
 module.exports = sendEmail;
