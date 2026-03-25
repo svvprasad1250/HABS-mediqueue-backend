97cb556f1e3aca1a48f9d19a6937770ddab18936
svvprasad1250 <venakataprasad5@gmail.com>
2026-03-25 20:51:06 +0530

    resend dev mail add

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,12 +3,14 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, htmlContent) => {
-    await resend.emails.send({
-        from: process.env.EMAIL_USER,
+    const response = await resend.emails.send({
+        from: "onboarding@resend.dev", // ✅ MUST USE THIS
         to: to,
         subject: subject,
         html: htmlContent
     });
+
+    console.log("Email response:", response); // 👈 add this for debug
 };
 
 module.exports = sendEmail;
