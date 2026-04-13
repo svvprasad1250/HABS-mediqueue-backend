de25d26fda2da2d057828d5a9d11a2fdf53624af
svvprasad1250 <venakataprasad5@gmail.com>
2026-04-13 20:03:43 +0530

    refrsh auth added

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -2,29 +2,23 @@ const asyncHandler = require("./asyncHandler");
 const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
-
     let token;
-
-    const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization || req.headers.Authorization;
 
     if (authHeader && authHeader.startsWith("Bearer ")) {
         token = authHeader.split(" ")[1];
-    }
-
-    else if (req.query.token) {
+    } else if (req.query.token) {
         token = req.query.token;
     }
 
     if (!token) {
         res.status(401);
-        throw new Error("No token provided");
+        throw new Error("User is not authorized or token is missing");
     }
 
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
-
         req.user = decoded.user;
-
         next();
     } catch (err) {
         res.status(401);
